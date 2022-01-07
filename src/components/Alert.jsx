cc6d9efe3e2da1300323ee3926f157bf5a1494df
Lee-You-Lim-github <akfl513@naver.com>
2022-01-07 15:51:50 +0900

    Danger 아이콘 구현

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,11 +1,13 @@
 // https://tailwind-elements.com/docs/standard/components/alerts/
 
 import Check from "./icons/Check";
+import Danger from "./icons/Danger";
 import Info from "./icons/Info";
 
 const MAPPING = {
   info: ["blue", <Info />],
   success: ["green", <Check />],
+  danger: ["red", <Danger />],
 };
 
 // type: info면 블루/ i아이콘을 씀.
@@ -29,7 +31,7 @@ function Alert({ type, message }) {
 // 속임수
 function Cheat() {
   return (
-    <div className="hidden bg-blue-100 text-blue-700 bg-green-100 text-green-700" />
+    <div className="hidden bg-blue-100 text-blue-700 bg-green-100 text-green-700 bg-red-100 text-red-700" />
   );
 }
 
