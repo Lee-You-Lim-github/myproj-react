fbb38ef22e255e1a7ef89b33979c6468067f3953
Lee-You-Lim-github <akfl513@naver.com>
2022-01-07 13:20:29 +0900

    Router / TopNav 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
+import ReviewList from "pages/reviews/ReveiwList";
+import Profile from "pages/accounts/Prefile";
+import Login from "pages/accounts/Login";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import TopNav from "components/TopNav";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div className="app">
+      <TopNav />
+      <Routes>
+        <Route pathe="/accounts/login" element={<Login />} />
+        <Route path="/accounts/profile" element={<Profile />} />
+        <Route path="reviews" element={<ReviewList />} />
+      </Routes>
     </div>
   );
 }
