4c38aa081f3021f91fbce711519082527798d9b7
Lee-You-Lim-github <akfl513@naver.com>
2022-01-10 18:20:44 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import TopNav from "components/TopNav";
 import Components from "pages/examples/Components";
 import PageReviewForm from "pages/reviews/PageReviewForm";
+import PageBlogList from "pages/blogs/PageBlogList";
 
 function App() {
   return (
     <div className="app">
       <TopNav />
       <Routes>
-        <Route path="/" element={<Navigate to="/reviews/" />} />
+        <Route path="/" element={<Navigate to="/blogs/" />} />
         <Route path="/accounts/login/" element={<Login />} />
         <Route path="/accounts/profile/" element={<Profile />} />
         <Route path="/reviews/" element={<ReviewList />} />
         <Route path="/reviews/new/" element={<PageReviewForm />} />
         <Route path="/reviews/:reviewId/edit/" element={<PageReviewForm />} />
         <Route path="/examples/components/" element={<Components />} />
+        <Route path="/blogs/" element={<PageBlogList />} />
       </Routes>
     </div>
   );
