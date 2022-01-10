e6dd86a8e5ccf3695805792376feb4de484a1034
Lee-You-Lim-github <akfl513@naver.com>
2022-01-10 23:48:18 +0900

    BlogForm에 useFieldValues 적용 전 값 저장 구현

--- src/components/blog/BlogForm.jsx
+++ src/components/blog/BlogForm.jsx
@@ -1,32 +1,60 @@
 import { useNavigate } from "react-router-dom";
+import DebugStates from "components/DebugStates";
+import { useState } from "react";
+import Axios from "axios";
 
 function BlogForm() {
+  const [fieldValues, setFieldValues] = useState({ title: "", content: "" });
   const navigate = useNavigate();
+
+  const handleFieldChange = (e) => {
+    const { name, value } = e.target;
+    setFieldValues((prevFieldValues) => ({
+      ...prevFieldValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    const url = "http://127.0.0.1:8000/blog/api/posts/";
+
+    Axios.post(url, fieldValues)
+      .then(() => navigate(`/blog/`))
+      .catch((error) => console.error(error));
+  };
+
   return (
-    <div class="rounded border-2 border-gray-300 p-3 my-3">
+    <div className="rounded border-2 border-gray-300 p-3 my-3">
       <div className="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2">제목</label>
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          제목
+        </label>
         <input
           className="shadow appe`arance-none border border-gray-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="title"
+          value={fieldValues.title}
+          onChange={(e) => handleFieldChange(e)}
         />
       </div>
       <div className="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2">콘텐츠</label>
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          콘텐츠
+        </label>
         <textarea
-          class="shadow appearance-none border border-gray-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border border-gray-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           name="content"
-          cols="30"
-          rows="10"
+          value={fieldValues.content}
+          onChange={(e) => handleFieldChange(e)}
         ></textarea>
       </div>
       <button
         className="shadow border bg-blue-100 hover:bg-blue-300 border-blue-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight"
-        onClick={() => navigate(`/blog/`)}
+        onClick={handleSubmit}
       >
         저장하기
       </button>
+      <DebugStates fieldValues={fieldValues} />
     </div>
   );
 }
