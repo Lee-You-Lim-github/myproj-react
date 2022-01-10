1e1b41ed5cdd53dd728f34c2005d6681bcc51f86
Lee-You-Lim-github <akfl513@naver.com>
2022-01-11 00:29:02 +0900

    BlogForm에 useFieldValues 훅을 적용하여 저장 구현

--- src/components/blog/BlogForm.jsx
+++ src/components/blog/BlogForm.jsx
@@ -1,28 +1,8 @@
 import { useNavigate } from "react-router-dom";
-import DebugStates from "components/DebugStates";
 import { useState } from "react";
 import Axios from "axios";
 
-function BlogForm() {
-  const [fieldValues, setFieldValues] = useState({ title: "", content: "" });
-  const navigate = useNavigate();
-
-  const handleFieldChange = (e) => {
-    const { name, value } = e.target;
-    setFieldValues((prevFieldValues) => ({
-      ...prevFieldValues,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    const url = "http://127.0.0.1:8000/blog/api/posts/";
-
-    Axios.post(url, fieldValues)
-      .then(() => navigate(`/blog/`))
-      .catch((error) => console.error(error));
-  };
-
+function BlogForm({ fieldValues, handleFieldChange, handleSubmit }) {
   return (
     <div className="rounded border-2 border-gray-300 p-3 my-3">
       <div className="mb-4">
@@ -50,11 +30,10 @@ function BlogForm() {
       </div>
       <button
         className="shadow border bg-blue-100 hover:bg-blue-300 border-blue-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight"
-        onClick={handleSubmit}
+        onClick={() => handleSubmit()}
       >
         저장하기
       </button>
-      <DebugStates fieldValues={fieldValues} />
     </div>
   );
 }
