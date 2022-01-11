9c6406f1e94e48456dd86913114c7f9a8de6f3a4
Lee-You-Lim-github <akfl513@naver.com>
2022-01-11 12:43:23 +0900

    digital clock 구현

--- src/App.js
+++ src/App.js
@@ -9,25 +9,33 @@ import PageReviewForm from "pages/reviews/PageReviewForm";
 import PageBlogList from "pages/blog/PageBlogList";
 import PageBlogDetail from "pages/blog/PageBlogDetail";
 import PageBlogForm from "pages/blog/PageBlogForm";
+import Clock from "pages/examples/Clock";
 
 function App() {
   return (
-    <div className="app">
-      <TopNav />
-      <Routes>
-        <Route path="/" element={<Navigate to="/blog/" />} />
-        <Route path="/accounts/login/" element={<Login />} />
-        <Route path="/accounts/profile/" element={<Profile />} />
-        <Route path="/reviews/" element={<ReviewList />} />
-        <Route path="/reviews/new/" element={<PageReviewForm />} />
-        <Route path="/reviews/:reviewId/edit/" element={<PageReviewForm />} />
-        <Route path="/examples/components/" element={<Components />} />
-        <Route path="/blog/" element={<PageBlogList />} />
-        <Route path="/blog/:postId/" element={<PageBlogDetail />} />
-        <Route path="/blog/new/" element={<PageBlogForm />} />
-        <Route path="/blog/:postId/edit/" element={<PageBlogForm />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <TopNav />
+        <Routes>
+          <Route path="/" element={<Navigate to="/blog/" />} />
+          <Route path="/accounts/login/" element={<Login />} />
+          <Route path="/accounts/profile/" element={<Profile />} />
+          <Route path="/reviews/" element={<ReviewList />} />
+          <Route path="/reviews/new/" element={<PageReviewForm />} />
+          <Route path="/reviews/:reviewId/edit/" element={<PageReviewForm />} />
+          <Route path="/examples/components/" element={<Components />} />
+          <Route path="/blog/" element={<PageBlogList />} />
+          <Route path="/blog/:postId/" element={<PageBlogDetail />} />
+          <Route path="/blog/new/" element={<PageBlogForm />} />
+          <Route path="/blog/:postId/edit/" element={<PageBlogForm />} />
+        </Routes>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/examples/clock/" element={<Clock />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
