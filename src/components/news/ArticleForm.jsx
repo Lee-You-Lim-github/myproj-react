9e0cda45bb3743312cdcfe4f836e8581affabbd8
Lee-You-Lim-github <akfl513@naver.com>
2022-01-13 11:16:13 +0900

    photo 업로드 구현 (수정x)

--- src/components/news/ArticleForm.jsx
+++ src/components/news/ArticleForm.jsx
@@ -44,8 +44,20 @@ function ArticleForm({ articleId, handleDidSave }) {
     e.preventDefault();
     console.log("submited !!!");
 
+    // fieldValues : 객체 (except 파일)
+    // 파일을 업로드하려면, FormDate 인스터스를 써야합니다.
+    const formData = new FormData();
+    Object.entries(fieldValues).forEach(([name, value]) => {
+      if (Array.isArray(value)) {
+        const fileList = value;
+        fileList.forEach((file) => formData.append(name, file));
+      } else {
+        formData.append(name, value);
+      }
+    });
+
     saveRequest({
-      data: fieldValues,
+      data: formData,
     }).then((response) => {
       const savedPost = response.data;
       if (handleDidSave) {
@@ -86,6 +98,20 @@ function ArticleForm({ articleId, handleDidSave }) {
             className="p-1 bg-gray-100 w-full h-80 outline-none focus:border focus:border-gray-400 focus:border-dashed"
           />
           {saveErrorMessages.content?.map((message, index) => (
+            <p key={index} className="text-xs text-red-400">
+              {message}
+            </p>
+          ))}
+        </div>
+        <div className="my-3">
+          <input
+            type="file"
+            accept=".png, .jpg, .jpeg"
+            multiple
+            name="photo"
+            onChange={handleFieldChange}
+          />
+          {saveErrorMessages.photo?.map((message, index) => (
             <p key={index} className="text-xs text-red-400">
               {message}
             </p>
