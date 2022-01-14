21bd2b625a145004c1f0c87551509dea648ba84d
Lee-You-Lim-github <akfl513@naver.com>
2022-01-14 14:30:56 +0900

    수정 / 삭제 버튼 만들기

--- src/components/tour/SpotDetail.jsx
+++ src/components/tour/SpotDetail.jsx
@@ -1,6 +1,9 @@
 import { useApiAxios } from "api/base";
+import Button from "components/Button";
 import DebugStates from "components/DebugStates";
+import LoadingIndicator from "components/LoadingIndicator";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SpotDetail({ spotId }) {
   const [
@@ -12,8 +15,12 @@ function SpotDetail({ spotId }) {
     refetch();
   }, []);
 
+  const navigate = useNavigate();
+
   return (
     <div>
+      {spotDetailLoading && <LoadingIndicator>로딩 중...</LoadingIndicator>}
+      {spotDetailError && "에러가 났어요!"}
       {spotDetail && (
         <div>
           <ul>
@@ -26,6 +33,14 @@ function SpotDetail({ spotId }) {
           </ul>
         </div>
       )}
+      <hr />
+      <div>
+        <Button onClick={() => navigate(`/tour/`)}>목록으로</Button>
+      </div>
+      <div>
+        <Button onClick={() => navigate(`/tour/${spotId}/edit/`)}>수정</Button>
+        <Button onClick={() => navigate(`/tour/`)}>삭제</Button>
+      </div>
 
       <DebugStates
         spotId={spotId}
