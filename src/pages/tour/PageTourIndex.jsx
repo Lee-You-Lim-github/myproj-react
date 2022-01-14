4c123b7860167501c1d3ee8cc1408e552c4f8c01
Lee-You-Lim-github <akfl513@naver.com>
2022-01-14 14:55:40 +0900

    새 Spot 버튼 구현

--- src/pages/tour/PageTourIndex.jsx
+++ src/pages/tour/PageTourIndex.jsx
@@ -1,10 +1,14 @@
+import Button from "components/Button";
 import H1 from "components/H1";
 import SpotList from "components/tour/SpotList";
+import { useNavigate } from "react-router-dom";
 
 function PageTourIndex() {
+  const navigate = useNavigate();
   return (
     <div>
       <H1>국내 여행 Spot</H1>
+      <Button onClick={() => navigate(`/tour/new/`)}>New Spot</Button>
       <SpotList />
     </div>
   );
--- src/pages/tour/PageTourSpotForm .jsx
+++ src/pages/tour/PageTourSpotForm .jsx
@@ -1,7 +1,11 @@
+import SpotForm from "components/tour/SpotForm";
+
 function PageTourSpotForm() {
   return (
     <div>
       <h2>추천 여행지 작성</h2>
+
+      <SpotForm />
     </div>
   );
 }
