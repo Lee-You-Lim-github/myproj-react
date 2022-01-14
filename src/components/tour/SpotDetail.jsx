0ad930e958d149d32a0421c83c620d4ecf73899c
Lee-You-Lim-github <akfl513@naver.com>
2022-01-14 14:18:58 +0900

    useApiAxios 데이터 가져오기/ 지역, 목적지, 설명, 사진 보여주기

--- src/components/tour/SpotDetail.jsx
+++ src/components/tour/SpotDetail.jsx
@@ -1,9 +1,38 @@
-function SpotDetail() {
+import { useApiAxios } from "api/base";
+import DebugStates from "components/DebugStates";
+import { useEffect } from "react";
+
+function SpotDetail({ spotId }) {
+  const [
+    { data: spotDetail, loading: spotDetailLoading, error: spotDetailError },
+    refetch,
+  ] = useApiAxios(`/tour/api/spots/${spotId}/`, { manual: true });
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
   return (
     <div>
-      <h2>몇 번째 여행지</h2>
+      {spotDetail && (
+        <div>
+          <ul>
+            <li>{spotDetail.area}</li>
+            <li>{spotDetail.destination}</li>
+            <li>{spotDetail.content}</li>
+            <li>
+              <img src={spotDetail.photo} alt={spotDetail.destination} />
+            </li>
+          </ul>
+        </div>
+      )}
 
-      <h2>상세 정보를 전부 보여줌.</h2>
+      <DebugStates
+        spotId={spotId}
+        spotDetail={spotDetail}
+        spotDetailLoading={spotDetailLoading}
+        spotDetailError={spotDetailError}
+      />
     </div>
   );
 }
